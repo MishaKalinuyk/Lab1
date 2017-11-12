8e51f59b02047c8eab83febdd7362a55943753d9
mac <mac@MacBook-Pro-mac.local>
2017-11-12 11:03:45 +0200

    Initial commit

---
+++ src/JavaClass1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mac on 12.11.17.
+ */
+public class JavaClass1 extends JavaClass2 {
+
+    public JavaClass1(double a, double b) {
+        super(a, b);
+    }
+
+    public double mult() {
+        return a * b;
+    }
+
+    public double div() {
+        return a / b;
+    }
+
+}
